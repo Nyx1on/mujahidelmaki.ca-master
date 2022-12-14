ee4016800ac26d073dc42e0a9780abf2605fd851
MΛKI <mo_elmaki01@hotmail.com>
2022-12-14 13:07:46 -0500

    adding an about me page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+const About = () => {
+  return (
+    <div>
+      <h1> About Me</h1>
+      <p> My name is Jane Doe and I am a web developer.</p>
+    </div>
+  )
+}
+
+export default About
